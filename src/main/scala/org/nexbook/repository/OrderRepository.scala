838736e9fdeca2f78843cb16cd2a1fb6efe19475
Bartosz Milczarek <bartosz@milczarek.it>
2015-12-08 13:52:04 +0100

    - order repository fix - correct timestamp desc ordering... reused ordering defined at OrderOrdering object

--- src/main/scala/org/nexbook/repository/OrderRepository.scala
+++ src/main/scala/org/nexbook/repository/OrderRepository.scala
@@ -1,12 +1,13 @@
 package org.nexbook.repository
 
 import org.nexbook.domain.Order
+import org.nexbook.utils.OrderOrdering
 
-import scala.collection.mutable.TreeSet
+import scala.collection.mutable
 
 class OrderRepository {
-  val timestampDescOrdering = Ordering.fromLessThan[Order](_.timestamp isBefore _.timestamp)
-  val orders = TreeSet.empty(timestampDescOrdering)
+  val timestampDescOrdering = OrderOrdering.timestampDesc
+  val orders = mutable.TreeSet.empty(timestampDescOrdering)
 
   def add(order: Order) = orders += order
 
