73896e2d2e26f789ae55f43804450f474dd94cbf
milczu <bartosz@milczarek.it>
2016-01-07 03:36:57 +0100

    - added ApplicationRunner - improvements in performance tests

---
+++ src/test/scala/org/nexbook/performance/PerformanceTest.scala
@@ -0,0 +1,24 @@
+package org.nexbook.performance
+
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.scalatest.concurrent.Timeouts
+import org.scalatest.{Matchers, WordSpecLike}
+
+/**
+  * Created by milczu on 07.01.16.
+  */
+trait PerformanceTest extends WordSpecLike with Matchers with Timeouts {
+
+  val counter = new AtomicInteger
+
+  def asyncExecute(f: => Any): Thread = asyncExecute(s"async-executor-${counter.incrementAndGet}")(f)
+
+  def asyncExecute(threadName: String)(f: => Any): Thread = {
+	val thread = new Thread(new Runnable {
+	  override def run(): Unit = f
+	})
+	thread.start()
+	thread
+  }
+}
